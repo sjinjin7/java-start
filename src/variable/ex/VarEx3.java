acd0ef4622864c450f82a1dba4f1d78fd39b8ffa
SeoJin <sjinjin7@gmail.com>
2025-08-06 04:46:19 +0900

    15. 문제와 풀이

---
+++ src/variable/ex/VarEx3.java
@@ -0,0 +1,12 @@
+package variable.ex;
+
+public class VarEx3 {
+
+    public static void main(String[] args){
+        long a = 10000000000L;
+        boolean b = true;
+        System.out.println("변수 a = " + a);
+        System.out.println("변수 b = " + b);
+    }
+
+}
