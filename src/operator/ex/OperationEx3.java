0cae3f348069a3d35d76e6527902a4740eb555e0
SeoJin <sjinjin7@gmail.com>
2025-08-10 06:57:37 +0900

    24. 문제와 풀이

---
+++ src/operator/ex/OperationEx3.java
@@ -0,0 +1,9 @@
+package operator.ex;
+
+public class OperationEx3 {
+    public static void main(String[] args) {
+        int score = 80;
+        System.out.println(score >= 80 && score <= 100);
+
+    }
+}
